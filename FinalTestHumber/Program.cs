0c9c126e0cedfa5adee8a37421e795c896af7686
Mayuri.Vaidya13 <Mayuri.Vaidya13@gmail.com>
2020-11-19 20:27:54 -0500

    Final test humber

---
+++ FinalTestHumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FinalTestHumber
+{
+    class Person  // Base class (parent) 
+    {
+        public void PersonName()
+        {
+            Console.WriteLine("Person Name");
+        }
+    }
+
+    class Adult : Person  // Derived class (child) 
+    {
+        public void FirstName()
+        {
+            Console.WriteLine("first name :Adult");
+        }
+        public void LastName()
+        {
+            Console.WriteLine("Last Name :Adult");
+        }
+
+    }
+    class Child : Person  // Derived class (child) 
+    {
+        public void FirstName()
+        {
+            Console.WriteLine("First Name :Child");
+        }
+        public void LastName()
+        {
+            Console.WriteLine("Last Name Child");
+        }
+
+    }
+}
+
